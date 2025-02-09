e1ea886856568194728813f9e34b014b25f696fe
unknown <merkoncel@gmail.com>
2025-02-09 17:55:15 +0300

    docker

--- SPA/proxy.conf.js
+++ SPA/proxy.conf.js
@@ -1,8 +1,7 @@
 ﻿const { createProxyMiddleware } = require('http-proxy-middleware');
-
 module.exports = {
   '/api': {
-    target: 'https://localhost:44372',
+    target: 'http://api:44372', // Используем имя сервиса из docker-compose.yml
     secure: false,
     logLevel: 'debug',
     changeOrigin: true
