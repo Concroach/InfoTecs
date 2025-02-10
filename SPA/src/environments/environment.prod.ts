94a9931756700fecdd9743face68b5226eac6762
unknown <merkoncel@gmail.com>
2025-02-10 15:34:47 +0300

    docker

--- SPA/src/environments/environment.prod.ts
+++ SPA/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 ﻿export const environment = {
   production: true,
-  apiUrl: 'http://localhost:44372/api' // Внешний адрес backend'а
+  apiUrl: 'http://localhost:8080/api' // Внешний адрес backend'а
 };
